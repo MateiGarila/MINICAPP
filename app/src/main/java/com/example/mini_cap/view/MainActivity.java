9450989bc694c4f1f3454067352ee23dc4be33d7
Matei Garila <mateigarila@hotmail.com>
2023-07-02 18:21:09 -0400

    Initial project commit

--- app/src/main/java/com/example/mini_cap/view/MainActivity.java
+++ app/src/main/java/com/example/mini_cap/view/MainActivity.java
@@ -2,15 +2,38 @@ package com.example.mini_cap.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mini_cap.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected TextView mainView;
+    protected Button toSessionActivity;
+    protected Button toStatsActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainView = findViewById(R.id.mainTextView);
+        toSessionActivity = findViewById(R.id.toSessionActivity);
+        toStatsActivity = findViewById(R.id.toStatsActivity);
+
+        toSessionActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    public void setToSessionActivity(){
+        Intent intent = new Intent(this, SessionActivity.class);
+        startActivity(intent);
     }
 }
