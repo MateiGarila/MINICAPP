9520dfff7846bbabc66eb42166326e3aa2e1b431
Matei Garila <mateigarila@hotmail.com>
2023-07-02 18:21:09 -0400

    SESS-1 | Create Session Activity - SessionActivity's layout has been added, conform to specifications

--- app/src/main/java/com/example/mini_cap/view/SessionActivity.java
+++ app/src/main/java/com/example/mini_cap/view/SessionActivity.java
@@ -1,16 +1,34 @@
 package com.example.mini_cap.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mini_cap.R;
 
 public class SessionActivity extends AppCompatActivity {
 
+    protected TextView mainTextView, statusTextView;
+    protected RecyclerView displayUser;
+    protected Button startStop, addUser, editUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
+
+        mainTextView = findViewById(R.id.sessionActivityTextView);
+        statusTextView = findViewById(R.id.sessionStatusTextView);
+        displayUser = findViewById(R.id.sessionUserDisplayRV);
+        startStop = findViewById(R.id.startStopSessionBTN);
+        addUser = findViewById(R.id.addUserBTN);
+        editUser = findViewById(R.id.editUserBTN);
+
+        //Temporary until we figure out a better way to navigate - Mat
+        mainTextView.setOnClickListener(v -> finish());
+
     }
 }
