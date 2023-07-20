6f9d5b8c8619d244fa5d400795224a5741197e64
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-07-21 03:51:14 +0800

    Dependency: SAPI 1.1
    Source repo commit: 62bd4e565f64e1742e59c3703f596f3889b6a26b

---
+++ app/src/main/java/app/uvtracker/sensor/pii/connection/application/ISensorConnection.java
@@ -0,0 +1,11 @@
+package app.uvtracker.sensor.pii.connection.application;
+
+import app.uvtracker.sensor.pii.connection.shared.IConnectable;
+
+public interface ISensorConnection extends IConnectable {
+
+    // Emits event: NewSampleReceivedEvent
+
+    // TODO: more features
+
+}
