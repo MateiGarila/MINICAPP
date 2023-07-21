eabd1447286d433a176e4c64557c37ae9965cc1b
Matei Garila <mateigarila@hotmail.com>
2023-07-21 12:36:28 -0400

    Removed old artifacts

--- app/src/main/java/com/example/mini_cap/controller/Dict.java
+++ app/src/main/java/com/example/mini_cap/controller/Dict.java
@@ -4,14 +4,10 @@ public class Dict {
 
     public static final String DATABASE_NAME = "mini-cap-db";
     public static final int DATABASE_VERSION = 1;
-
     public static final String TABLE_PRESET = "user";
     public static final String COLUMN_PRESET_ID = "userID";
     public static final String COLUMN_PRESET_NAME = "name";
     public static final String COLUMN_PRESET_AGE = "age";
     public static final String COLUMN_PRESET_SKINTONE = "skinTone";
-
-    public static final int CREATE_PRESET = 1;
-    public static final int EDIT_PRESET = 2;
 
 }
