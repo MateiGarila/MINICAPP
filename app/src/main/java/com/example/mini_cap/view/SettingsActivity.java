6bccc26136498999a8c5b565acb5c045502d4f80
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-08-03 06:41:20 +0800

    Migrated connection flow to sensor controller

--- app/src/main/java/com/example/mini_cap/view/SettingsActivity.java
+++ app/src/main/java/com/example/mini_cap/view/SettingsActivity.java
@@ -16,11 +16,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.example.mini_cap.R;
+import com.example.mini_cap.controller.SensorController;
+import com.example.mini_cap.view.helper.IntentDataHelper;
 
 import app.uvtracker.data.optical.OpticalRecord;
 import app.uvtracker.sensor.SensorAPI;
@@ -39,52 +42,24 @@ public class SettingsActivity extends AppCompatActivity implements IEventListene
     protected Button connectBTN;
     private IScanner iScanner;
     private ISensor iSensor;
-    private final int ENABLE_BLUETOOTH_REQUEST_CODE = 1;
+
+    private SensorController sensorController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        this.sensorController = new SensorController(this);
+        IntentDataHelper.writeSensorController(this.sensorController);
+
         //Attaching the UI elements to their respective objects
         //mainView = findViewById(R.id.mainTextView);
         connectBTN = findViewById(R.id.connectBTN);
         cityName = findViewById(R.id.cityInput);
         search = findViewById(R.id.search);
-
-
-        if(!hasRequiredRuntimePermissions())
-            requestRunTimePermissions();
-
-        if(this.iScanner == null) {
-            try {
-                this.iScanner = SensorAPI.getScanner(this);
-            } catch (TransceiverException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        this.iScanner.registerListener(this);
-
-        connectBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                try {
-                    iScanner.startScanning();
-                    (new Handler(Looper.getMainLooper()))
-                            .postDelayed(() -> {
-                                try {
-                                    iScanner.stopScanning();
-                                } catch (TransceiverException ignored) {
-
-                                }
-                            }, 10000);
-                } catch (TransceiverException e) {
-                    Toast.makeText(SettingsActivity.this, "Sensor oopsie", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        
+        connectBTN.setOnClickListener((v) -> this.sensorController.connectToAnySensor());
 
         search.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -101,66 +76,6 @@ public class SettingsActivity extends AppCompatActivity implements IEventListene
                 }
             }
         });
-
-    }
-
-
-    @EventHandler
-    public void onScanUpdate(SensorScannedEvent event) {
-        try {
-            iScanner.stopScanning();
-        } catch (TransceiverException e) {
-            Toast.makeText(SettingsActivity.this, "Sensor oopsie", Toast.LENGTH_SHORT).show();
-        }
-        if(iSensor != null) return;
-        iSensor = event.getSensor();
-        iSensor.getConnection().registerListener(this);
-        iSensor.getConnection().connect();
-        Toast.makeText(SettingsActivity.this, "Connecting..", Toast.LENGTH_SHORT).show();
-    }
-
-    @EventHandler
-    public void onSensorConnectionStatusUpdate(ConnectionStateChangeEvent event) {
-        if(event.getStage() != ConnectionStateChangeEvent.State.ESTABLISHED) return;
-        Toast.makeText(SettingsActivity.this, "Connection established", Toast.LENGTH_SHORT).show();
-    }
-/*
-    @EventHandler
-    public void onNewSample(NewSampleReceivedEvent event) {
-        OpticalRecord record = event.getRecord();
-        float illuminance = record.illuminance;
-        float uvIndex = record.uvIndex;
-        sensorView.setText(String.format("Light: %1$.1f, UVI: %2$.2f", illuminance, uvIndex));
-    }
-*/
-    public boolean hasPermission(String permissionType) {
-        return ContextCompat.checkSelfPermission(this, permissionType) ==
-                PackageManager.PERMISSION_GRANTED;
-    }
-
-    public boolean hasRequiredRuntimePermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            return hasPermission(Manifest.permission.BLUETOOTH_SCAN) &&
-                    hasPermission(Manifest.permission.BLUETOOTH_CONNECT);
-        } else {
-            return hasPermission(Manifest.permission.ACCESS_FINE_LOCATION);
-        }
-    }
-
-    public void requestRunTimePermissions(){
-
-        String[] permission;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            permission = new String[] {
-                    Manifest.permission.BLUETOOTH_SCAN,
-                    Manifest.permission.BLUETOOTH_CONNECT
-            };
-        } else {
-            permission = new String[] {
-                    Manifest.permission.ACCESS_FINE_LOCATION
-            };
-        }
-        ActivityCompat.requestPermissions(this, permission, 1);
 
     }
 
