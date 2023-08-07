fcd67e02b55800dc45cbd9245acab2ecae4de994
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-08-08 05:59:49 +0800

    Removed test views

--- app/src/main/java/com/example/mini_cap/view/SessionActivity.java
+++ app/src/main/java/com/example/mini_cap/view/SessionActivity.java
@@ -1,26 +1,16 @@
 package com.example.mini_cap.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
-import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mini_cap.R;
 import com.example.mini_cap.controller.DBHelper;
